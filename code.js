b4d9cacbec281d8b58a107a07e81d9094be5dd62
irivas117 <irivas.sh@mysummitps.org>
2016-06-06 22:18:43 +0000

    update

--- code.js
+++ code.js
@@ -5,12 +5,12 @@ var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+    info1.innerHTML = "Andre Iguodala is a very versitile player and is valuable to the Golden State Warrior team. He is a lock down defender and comes off the benc. Andre Iguodala first started his NBA career on the Philidelphia 76ers. He was drafted 9th overall in 2004. When he was younger he was a very quick and fast player. He was known for his passing, intelligence on the court, athletisim, and lock down defense. Andre Iguodala is known as a wing player, point forward, and lockdown defender. Andre is now the age of 31 and is still one of the better players known in the NBA today. He is a 1 time NBA all star in 2012, has 1 NBA champinoship with the Golden State Warriors has a chance at 2 this year, All defensive 1st team and second team, and finals MVP. These are just some of the many different accomplishments that a great player can make during the coruse of their NBA career. ";
+    info2.innerHTML = "";
 });
                        
 item2.addEventListener("click", function(){
-    info1.innerHTML = "Item 2 stuff";
-    info2.innerHTML = "Item 2 other stuff";
+    info1.innerHTML = "John Wall is a very good point guard in the NBA. He was drafted in 2011 as the first pick in the draft. He was one of the top prospects. He attended Kentuckey Statae College. He played along side current NBA players Eric Bledsoe and Demarcus Cousins. He is a 2 time all star, all rookie first team, and all defensive second team. He is a very young player, only about the age of 25 and still has mnay more years to accomplish a lot in his career. John Wall is one of the most versitile point guards in the league. He is a very good defender, not the best outside shooter but can still hit the shot if he is open. He is one of the best passers and playmakers in the NBA. He averages 18 points a game, with 10.2 assists a game, and 4.9 rebounds a game.";
+    info2.innerHTML = "";
 });
                        
